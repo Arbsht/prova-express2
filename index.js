28a91a3d4bbfff4092fba8e939d0651075894fd9
arb sht <shtjefni.arbi@iisgalvanimi.edu.it>
2024-03-10 13:10:36 +0000

    il resto

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get('/', function (req, res) {
 app.get('/profili', function (req, res) {
     res.sendFile(path.join(__dirname, 'profili.html'));
 });
+app.get('/profili/tom', function (req, res) {
+  res.sendFile(path.join(__dirname, './profili/tom.html'));
+  app.use(express.static(__dirname + '/profili'));
+});
+app.get('/profili/sarah', function (req, res) {
+  res.sendFile(path.join(__dirname, './profili/sarah.html'));
+  app.use(express.static(__dirname + '/profili'));
+});
+app.get('/profili/caroline', function (req, res) {
+  res.sendFile(path.join(__dirname, './profili/caroline.html'));
+  app.use(express.static(__dirname + '/profili'));
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
